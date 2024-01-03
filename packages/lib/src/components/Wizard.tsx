17e7d082429b9321ae9c004ff535cee52a60c68c
minnzee <minnzee@syncswap.xyz>
2024-01-04 06:58:50 +0800

    update: use syncswap modal

--- packages/lib/src/components/Wizard.tsx
+++ packages/lib/src/components/Wizard.tsx
@@ -1,5 +1,5 @@
 import { styled } from "@mui/material";
-import React, { useCallback, useMemo } from "react";
+import React, { useCallback, useEffect, useMemo } from "react";
 import { useWizardStore, WizardAction, WizardActionStatus } from "../store";
 import { StyledColumnFlex, StyledText } from "../styles";
 import { Modal, Spinner } from "./base";
@@ -7,18 +7,113 @@ import { Modal, Spinner } from "./base";
 import { FaCheckCircle } from "@react-icons/all-files/fa/FaCheckCircle";
 import { MdError } from "@react-icons/all-files/md/MdError";
 
+// import { States } from "./../../../../../../states/dispatcher/states"
+// import { TransactionStatus } from "../../../../../../states/data/transactionStatus";
+import { TransactionHandlers } from "../../../../../../requests"
+import { stateStore, updateStateStore } from "../../../../../../states/stateStore";
+import { TransactionWindowContentBuilder } from "../../../../../../components/windows/transactionStatusWindow";
+import { TransactionWindow } from "../../../../../../states/dispatcher/transactionWindow";
+import { States } from "../../../../../../states/dispatcher/states";
+import { TransactionStatus } from "../../../../../../states/data/transactionStatus";
 export function Wizard() {
   const store = useWizardStore();
   const content = useContent();
+
+  useEffect(() => {
+    console.log('wizard action:', store.action, 'wizard status:', store.status, 'wizard error:', store.error)
+    setTranscationWindow();
+
+  }, [store])
+
+  function setTranscationWindow() {
+    let errorMsg, pendingMsg, successMsg;
+    
+    switch (store.status) {
+      case WizardActionStatus.PENDING:
+        pendingMsg = "Pending";
+        if (store.action) {
+          switch (store.action) {
+            case WizardAction.APPROVE:
+              pendingMsg = "Approving";
+              break;
+            case WizardAction.CREATE_ORDER:
+              pendingMsg = "Submitting order";
+              break;
+            case WizardAction.WRAP:
+              pendingMsg = "Wrapping";
+              break;
+            case WizardAction.UNWRAP:
+              pendingMsg = "Unwrapping";
+              break;
+          }
+        }
+        TransactionWindow.render(TransactionWindowContentBuilder.create(pendingMsg));
+        break;
+      case WizardActionStatus.ERROR:
+        errorMsg = store.error;
+        if (!store.error) {
+          switch (store.action) {
+            case WizardAction.APPROVE:
+              errorMsg = "Approval failed";
+              break;
+            case WizardAction.CREATE_ORDER:
+              errorMsg = "Submit order failed";
+              break;
+            case WizardAction.WRAP:
+              errorMsg = "Wrap failed";
+              break;
+            case WizardAction.UNWRAP:
+              errorMsg = "Unwrap failed";
+              break;
+          }
+        }
+
+        TransactionHandlers.createErrorHandler()(errorMsg);
+        break;
+
+      case WizardActionStatus.SUCCESS:
+        successMsg = "Operation successful";
+        switch (store.action) {
+          case WizardAction.APPROVE:
+            successMsg = "Approval successful";
+            break;
+          case WizardAction.CREATE_ORDER:
+            successMsg = "Order submitted successfully";
+            break;
+          case WizardAction.WRAP:
+            successMsg = "Wrap successful";
+            break;
+          case WizardAction.UNWRAP:
+            successMsg = "Unwrap successful";
+            break;
+        }
+        // TransactionHandlers.createErrorHandler()(successMsg);
+        stateStore().currentTransaction.windowContent = {
+          main: successMsg,
+          subtitle: ''
+        }
+        States.setTransactionStatus(TransactionStatus.CUSTOM_SUCCESSED);
+        break;
+
+      default:
+
+        break;
+    }
+    updateStateStore();
+  }
 
   const onClose = useCallback(() => {
     store.setOpen(false);
   }, []);
 
   return (
-    <Modal open={store.open} onClose={onClose}>
-      <StyledContainer>{content}</StyledContainer>
-    </Modal>
+    <div style={{ display: 'none' }}>
+      {false && <Modal open={store.open} onClose={onClose} >
+        <StyledContainer>{content}</StyledContainer>
+      </Modal>}
+    </div>
+
+    // <></>
   );
 }
 
